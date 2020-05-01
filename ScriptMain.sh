198e09413b4860462abd05f8f1b842d380e6f89c
Mohamed Ali Jaziri <48095666+MedDali-Jaziri@users.noreply.github.com>
2020-05-01 22:33:13 +0200

    Update ScriptMain.sh

--- ScriptMain.sh
+++ ScriptMain.sh
@@ -1,6 +1,6 @@
 echo "remove the file to server"
-scp -r Just-Book-Store-WebSite-2/Partie_MYSQL/ useradm@192.168.56.60:/home/useradm
-scp -r Just-Book-Store-WebSite-2/Partie_PHP_APACHE2 useradm@192.168.56.20:/home/useradm
+scp -r Partie_MYSQL/ useradm@192.168.56.60:/home/useradm
+scp -r Partie_PHP_APACHE2 useradm@192.168.56.20:/home/useradm
 
 echp "Open terminal automaticly"
 gnome-terminal -e "bash -c 'ssh useradm@192.168.56.20;exec $SHELL'"
