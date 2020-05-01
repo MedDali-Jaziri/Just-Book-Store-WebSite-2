14047d32e01e28d0c0920c911b0eeed6b12ccc7e
Mohamed Ali Jaziri <48095666+MedDali-Jaziri@users.noreply.github.com>
2020-05-01 22:21:55 +0200

    Update ScriptMain.sh

--- ScriptMain.sh
+++ ScriptMain.sh
@@ -1,8 +1,8 @@
-git clone https://github.com/MedDali-Jaziri/Just-Book-Store-WebSite-2.git
+echo "remove the file to server"
 scp -r Just-Book-Store-WebSite-2/Partie_MYSQL/ useradm@192.168.56.60:/home/useradm
 scp -r Just-Book-Store-WebSite-2/Partie_PHP_APACHE2 useradm@192.168.56.20:/home/useradm
 
-
+echp "Open terminal automaticly"
 gnome-terminal -e "bash -c 'ssh useradm@192.168.56.20;exec $SHELL'"
 gnome-terminal -e "bash -c 'ssh useradm@192.168.56.60;exec $SHELL'"
 
